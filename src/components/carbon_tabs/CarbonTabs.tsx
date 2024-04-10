6584a5a7362004c27c36be6e196ea754bb5117ac
dannymunozramirez <danny.munoz.ramirez@gmail.com>
2024-04-10 19:14:02 -0400

    Added new tenant, owner forms, tabs and functionalities

--- src/components/carbon_tabs/CarbonTabs.tsx
+++ src/components/carbon_tabs/CarbonTabs.tsx
@@ -1,45 +1,96 @@
-import { Dashboard, CloudMonitoring, Activity, Settings, Home, Analytics } from "@carbon/icons-react";
-import { Button, Checkbox, Search, Tab, TabList, TabPanel, TabPanels, Tabs, TextInput } from "@carbon/react";
-import { useUser } from "../../hooks/useUser";
-import { ImmDashboard } from "../../screens/dashboard/ImmDashboard";
-import { CarbonHeader } from "../carbon_header/CarbonHeader";
+import React, { useEffect, useState } from 'react';
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@carbon/react';
+import { useCreate } from '../../hooks/useCreate';
+import { useUser } from '../../hooks/useUser';
+import { CarbonHeader } from '../carbon_header/CarbonHeader';
+import { CarbonHomeTabs } from './CarbonHomeTabs';
+import { Close, Home } from '@carbon/icons-react';
+import { TabInfo } from '../../interfaces/UserInterface';
 
+interface Props {
+    contained?: any;
+}
 
-export const CarbonTabs = () => {
+export const CarbonTabs = ({ contained }: Props) => {
+
+    const [selectedIndex, setSelectedIndex] = useState<number>(0); // Track selected tab index
     const { userLoggedGlobal } = useUser();
+    const { addedTab, onRemovingTabs } = useCreate();
+
+    const emptyTabList: TabInfo[] = [
+        {
+            label: 'Home',
+            panel: <CarbonHomeTabs />,
+            icon: () => <Home size={17} />,
+            disabled: false
+        },
+    ];
+    const [tabList, setTabList] = useState<TabInfo[]>(emptyTabList);
+
+
+    useEffect(() => {
+        if (addedTab.length > 0) {
+            let filteredList = addedTab.filter(item => item.label != '')
+            const newList = [...emptyTabList, ...filteredList];
+            setTabList(newList);
+            setSelectedIndex(newList.length - 1);
+        }
+
+    }, [addedTab.length]);
+
+    // Function to handle tab selection
+    const handleTabSelection = (index: number) => {
+        setSelectedIndex(index);
+    };
+
+    const removeTab = (tabItem: TabInfo) => {
+        // Create a copy of the tabList array
+        const listItemsRemoved = tabList.filter(item => item !== tabItem);
+        // Update the state with the modified array
+        setTabList(listItemsRemoved);
+        // Call the onRemovingTabs function with the removed tabItem
+        onRemovingTabs(tabItem);
+    };
 
     return (
         <>
             <div style={{ marginBottom: '45px' }}>
-                <CarbonHeader name={userLoggedGlobal.name} /></div>
+                <CarbonHeader name={userLoggedGlobal.name} />
+            </div>
             <div style={{ backgroundColor: 'whitesmoke' }}>
-                <Tabs >
-                    <TabList aria-label="List of tabs" activation="manual">
-                        <Tab renderIcon={() => <Home size={15} />}/>
-                        <Tab renderIcon={() => <Dashboard size={15} />}>Dashboard</Tab>
-                        <Tab renderIcon={() => <CloudMonitoring size={15} />}>Monitoring</Tab>
-                        <Tab renderIcon={() => <Activity size={15} />}>Activity</Tab>
-                        <Tab renderIcon={() => <Analytics size={15} />}>Analyze</Tab>
-                        <Tab renderIcon={() => <Settings size={15} />} >Settings</Tab>
+                <Tabs selectedIndex={selectedIndex}>
+                    <TabList contained={contained} aria-label="List of tabs" activation="manual">
+                        {tabList.map((tab, index) => (
+                            tab.label === 'Home' ? (
+                                <Tab
+                                    key={index}
+                                    style={{ backgroundColor: 'white' }}
+                                    renderIcon={tab.icon}
+                                    aria-current={index === selectedIndex ? "page" : undefined} // Apply aria-current to the selected tab
+                                    onClick={() => handleTabSelection(index)}
+                                >
+                                    {tab.label}
+                                </Tab>
+                            ) : (
+                                <Tab
+                                    key={index}
+                                    style={{ backgroundColor: 'white' }}
+                                    renderIcon={() => <Close onClick={() => removeTab(tab)} />}
+                                    aria-current={index === selectedIndex ? "page" : undefined} // Apply aria-current to the selected tab
+                                    onClick={() => handleTabSelection(index)}
+                                >
+                                    {tab.label}
+                                </Tab>
+                            )
+                        ))}
                     </TabList>
                     <TabPanels>
-                        <TabPanel><ImmDashboard /></TabPanel>
-                        <TabPanel>
-                            <form style={{ margin: '2em' }}>
-                                <legend className={`cds--label`}>Validation example</legend>
-                                <Checkbox id="cb" labelText="Accept privacy policy" />
-                                <Button style={{ marginTop: '1rem', marginBottom: '1rem' }} type="submit">
-                                    Submit
-                                </Button>
-                                <TextInput type="text" labelText="Text input label" helperText="Optional help text" id="text-input-1" />
-                            </form>
-                        </TabPanel>
-                        <TabPanel>Tab Panel 3</TabPanel>
-                        <TabPanel>Tab Panel 4</TabPanel>
-                        <TabPanel>Tab Panel 5</TabPanel>
+                        {tabList.map((tab, index) => (
+                            <TabPanel style={{ backgroundColor: 'white' }} key={index}>{tab.panel}</TabPanel>
+                        ))}
                     </TabPanels>
                 </Tabs>
             </div>
         </>
-    )
-}
+    );
+};
